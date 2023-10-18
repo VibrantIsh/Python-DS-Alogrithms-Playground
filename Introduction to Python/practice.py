62f007155060cbc2638a4bbc9c3b6cee739199bd
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-10-18 23:53:50 +0530

    practice.py

---
+++ Introduction to Python/practice.py
@@ -0,0 +1,6 @@
+#E-1.1 Write a program to prompt a user to enter his/her name and mailing address and display the entered information on the screen.
+name = input("Enter your name: ")
+address = input("Enter your mailing address: ")
+
+print("\nName: " + name)
+print("Mailing Address: " + address)
