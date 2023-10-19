5b91ac9fe135106c234a6c3570d91720faaeca68
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-10-19 23:33:44 +0530

    Update practice.py

--- Introduction to Python/practice.py
+++ Introduction to Python/practice.py
@@ -105,3 +105,10 @@ num2 = int(input("Enter the second number: "))
 result = gcd_recursive(num1, num2)
 print(f"The GCD of {num1} and {num2} is {result}")
 
+# using math module
+import math 
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+gcd = math.gcd(num1, num2)
+print(gcd)
+
