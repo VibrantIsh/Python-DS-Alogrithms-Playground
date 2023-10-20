470efd7f8349580e491f7e001e7931be989d8309
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-10-20 15:38:46 +0530

    exercises.py

--- Introduction to Python/exercises.py
+++ Introduction to Python/exercises.py
@@ -1,1 +1,10 @@
-
+# R-1.1 Write a short Python function, is multiple (n, m), that takes two integer values and returns True if n is a multiple of m, that is, n = mi for some integer i, and False otherwise.
+def multiple(n, m):
+    if n % m == 0:
+        return True
+    else:
+        return False
+n = 10
+m = 2
+check = multiple(n, m)
+print(f"{n} is a multiple of {m}: {check}
