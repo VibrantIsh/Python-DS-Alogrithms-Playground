c31e8cd446144ca17ac2277889484a7c3b74e18a
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-10-20 23:49:58 +0530

    creativity.py

--- Introduction to Python/creativity.py
+++ Introduction to Python/creativity.py
@@ -1,1 +1,7 @@
+#C-1.10 Write a prudocode description of a function that reverses a list of integers, so that the numbers are listed in the opposite onder than they were before, and compare this method.
+function reverseList(originalList):
+   create an empty list called reversedList
+   for each item in the original list starting from the end:
+      append this item to reversedList
+   return reversedList
 
