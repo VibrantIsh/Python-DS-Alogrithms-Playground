6f522061b6eb0830d0ea30f96426ad1d335aa3b3
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-10-21 00:49:20 +0530

    creativity.py

--- Introduction to Python/creativity.py
+++ Introduction to Python/creativity.py
@@ -5,3 +5,13 @@ function reverseList(originalList):
       append this item to reversedList
    return reversedList
 
+#C-1.11 Write a short Python function that takes a sequence of integer values and determines if there is a distinct pair of consecutive numbers in the sequence whose product is even.
+def is_even_product(lst):
+    for i in range(len(lst) -1):
+        current_num = lst[i]
+        next_num = lst [i + 1]
+        if (current_num * next_num ) % 2 == 0:
+            return True
+    else:
+        return False
+
