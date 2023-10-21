47838c76fd6934226e21f8cb72cc3ea6e033c273
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-10-21 20:43:42 +0530

    Update projects-1.24.py

--- Introduction to Python/projects-1.24.py
+++ Introduction to Python/projects-1.24.py
@@ -1,1 +1,11 @@
+# P-1.24 Write a Python program that can take a positive integer greater than 2 as input and write out the number of times one must repeatedly divide this number by 2 before getting a value less than 2.
+counter = 0
+num = int(input("Enter a number: "))
+if num <= 2:
+    print("Number must be greater than 2")
+else:
+    while num >= 2:
+        num = num / 2
+        counter += 1
 
+    print(counter)
