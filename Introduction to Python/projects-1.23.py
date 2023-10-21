b8645f10ff6841df7b36110d2a674b66bb84c6e4
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-10-21 18:20:32 +0530

    Update projects-1.23.py

--- Introduction to Python/projects-1.23.py
+++ Introduction to Python/projects-1.23.py
@@ -1,1 +1,18 @@
+#P-1.23 Write a python program that output all the possible strings formed by using the characters 'c', 'a', 't', 'd', 'o', and 'g' exactly once.
+
+
+import random
+
+characters = 'catdog'
+generated_strings = set()
+
+while True:
+    string_length = random.randint(1, 10)  # Random string length, maximum 10 characters
+    random_string = ''.join(random.choice(characters) for i in range(string_length))
+
+    if random_string not in generated_strings:
+        generated_strings.add(random_string)
+        print(random_string)
+        break
+
 
