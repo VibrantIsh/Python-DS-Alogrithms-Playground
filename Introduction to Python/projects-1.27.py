480051e028a388834b6475e38ccb6149fe38124d
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-10-22 01:22:06 +0530

    Update projects-1.27.py

--- Introduction to Python/projects-1.27.py
+++ Introduction to Python/projects-1.27.py
@@ -1,1 +1,1 @@
-
+# P-1.27 A common punishment for school children is to write out a sentence multiple times. Write a Python stand-alone program that will write out the following sentence one hundred times: "I will never spam my friends again." Your program it should make eight different random-looking typos. should number each of the sentences and
