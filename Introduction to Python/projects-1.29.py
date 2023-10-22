8ec67db175e036ecfdc0520f5c35454661b4e8f8
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-10-22 16:36:53 +0530

    Update projects-1.29.py

--- Introduction to Python/projects-1.29.py
+++ Introduction to Python/projects-1.29.py
@@ -1,1 +1,33 @@
-# P-1.29 Write a Python program that inputs a list of words, separated by whitespace, and outputs how many times each word appears in the list. You need not worry about efficiency at this point, however, as this topic is something that will be addressed later in this book.
+# P-1.29 Write a Python program that inputs a list of words, separated by whitespace, and outputs how many times each word appears in the list.
+
+sentence_input = input("Enter your sentence here: ")
+def functioning(word_list):
+    words = word_list.split()
+    word_dic = {}
+    for word in words:
+        if word in word_dic:
+            word_dic[word] += 1
+        else:
+            word_dic[word] = 1
+    return word_dic
+
+passing = functioning(sentence_input)
+
+for w, c in passing.items():
+    if c > 1:
+     print(f"{w} has: {c} count.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
