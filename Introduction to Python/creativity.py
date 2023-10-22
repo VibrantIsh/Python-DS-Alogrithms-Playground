7eacebea5fc8a205e07852ff5ea10cbeefabdcce
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-10-22 16:43:56 +0530

    Update creativity.py

--- Introduction to Python/creativity.py
+++ Introduction to Python/creativity.py
@@ -55,3 +55,12 @@ lines.reverse()
 for line in lines:
     print(line)
 
+#C-1.18 Give an example of a Python code fragment that attempts to write an element to a list based on an index that may be out of bounds. If that index is out of bounds, the program should catch the exception that results, and print the following error message: "Don't try buffer overflow attacks in Python!"
+my_list = [1, 2, 3, 4, 5]
+
+try:
+    index = 10  # This index is out of bounds
+    value = 100
+    my_list[index] = value
+except IndexError:
+    print("Don't try buffer overflow attacks in Python!")
