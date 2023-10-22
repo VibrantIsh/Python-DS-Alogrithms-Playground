bae8ea684fd8d41aa242a9d32da8ba3eea17708a
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-10-22 14:44:38 +0530

    Update and rename P-1.29 to projects-1.29.py

---
+++ Introduction to Python/projects-1.29.py
@@ -0,0 +1,1 @@
+# P-1.29 Write a Python program that inputs a list of words, separated by whitespace, and outputs how many times each word appears in the list. You need not worry about efficiency at this point, however, as this topic is something that will be addressed later in this book.
