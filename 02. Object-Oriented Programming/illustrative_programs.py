9faa25a335e059aba41819c1e2795aeacd0c9cd2
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-11-18 18:41:26 +0530

    Update illustrative_programs.py

--- 02. Object-Oriented Programming/illustrative_programs.py
+++ 02. Object-Oriented Programming/illustrative_programs.py
@@ -1,1 +1,17 @@
+# E-2.1 A class Customer contains the following details: customer name, customer ID, customer account number. Write a program that initializes the data members and displays them.
 
+#E-2.1 A class Customer contains the following details: customer name, customer ID, customer account number. Write a program that initializes the data members and displays them.
+class Customer:
+    def __init__(self, name, iden, acno):
+        self.custname = name 
+        self.identity = iden
+        self.accnumber = acno
+    def displat(self):
+        print("Customer name: ", self.custname)
+        print("Customer Identity", self.identity)
+        print("Account Numner", self.accnumber)
+name = input ("Enter name: ")
+iden = input("Identity number: ")
+acno = input ("Enter Account Number: ")
+obj = Customer(name, iden , acno)
+obj.displat()
