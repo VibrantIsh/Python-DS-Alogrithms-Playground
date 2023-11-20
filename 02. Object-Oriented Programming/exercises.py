f56e08c8d6ba4861f14bb18c91e635f6c87eb20a
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-11-21 01:31:21 +0530

    Update exercises.py

--- 02. Object-Oriented Programming/exercises.py
+++ 02. Object-Oriented Programming/exercises.py
@@ -194,3 +194,25 @@ d = u.m(v)
 print(d)
 
 
+# R-2.12 The Vector class of Section 2.3.3 provides a constructor that takes an integer d, and produces a d-dimensional vector with all coordinates equal to 0. Another convenient form for creating a new vector would be to send the constructor a parameter that is some iterable type representing a sequence of numbers, and to create a vector with dimen sion equal to the length of that sequence and coordinates equal to the sequence val- ues. For example, Vector ([4, 7, 51) would produce a three-dimensional vector with coordinates < 4 7, 5. Modify the constructor so that either of these forms is accept- able; that is, if a single integer is sent, it produces a vector of that dimension with all zeros, but if a sequence of numbers is provided, it produces a vector with coordinates based on that sequence.
+class Vec:
+    def __init__(self, arg):
+        try:
+            self.c = list(arg)
+            self.d = len(arg)
+        except TypeError:
+            self.d = arg
+            self.c = [0] * arg
+
+    def __repr__(self):
+        return f"Vec({self.c})"
+
+v1 = Vec(5)
+print(v1)
+
+v2 = Vec([4, 7, 5])
+print(v2)
+
+v3 = Vec((1, 2, 3, 4))
+print(v3)
+
