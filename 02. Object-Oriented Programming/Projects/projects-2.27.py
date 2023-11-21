46d5b439d3f55700e241fcc99acc238d1aa1e9bf
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-11-21 23:47:06 +0530

    Update projects-2.27.py

--- 02. Object-Oriented Programming/Projects/projects-2.27.py
+++ 02. Object-Oriented Programming/Projects/projects-2.27.py
@@ -1,1 +1,23 @@
+# P-2.27 Write a Python program that inputs a document and then outputs a barchart plot of the fre- quencies of each alphabet character that appears in that document.
+import matplotlib.pyplot as plt
+
+def freq(doc):
+    freqs = {}
+    for c in doc:
+        if c.isalpha():
+            c = c.lower()
+            freqs[c] = freqs.get(c, 0) + 1
+    return freqs
+
+def plot(freqs):
+    plt.bar(*zip(*freqs.items()))
+    plt.show()
+
+def main():
+    d = input()
+    p = freq(d)
+    plot(p)
+
+if __name__ == "__main__":
+    main()
 
