d52ed2734602d2cdae2c0f63640bc1dda9170317
Ishmeet <144150492+VibrantIsh@users.noreply.github.com>
2023-11-21 15:39:41 +0530

    Update creativity.py

--- 02. Object-Oriented Programming/creativity.py
+++ 02. Object-Oriented Programming/creativity.py
@@ -1,1 +1,1 @@
-
+# C-2.20 The Sequence Iterator class of Section 2.3.4 provides what is known as a forward iter. ator. Implement a class named Reversed sequence Iterator that serves as a reverse iterator for any Python sequence type. The first call to next should return the last element of the sequence, the second call to next should return the second-to-last element, and so forth.
